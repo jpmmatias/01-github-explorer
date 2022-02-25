8a63607ba90dc39d64303e9827a5dc0642be5ddb
João Pedro Matias <joao.matias@rebase.com.br>
2022-02-25 16:54:20 -0300

    Conectando com a API do github

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,13 +1,31 @@
 import { RepositoryItem } from './RepositoryItem';
+import '../styles/repositories.scss';
+import { useState, useEffect } from 'react';
+
+// https://api.github.com/orgs/rocketseat/repos
 
 export function RepositoryList() {
+	const [repositories, setRepositories] = useState([]);
+
+	useEffect(() => {
+		fetch('https://api.github.com/orgs/rocketseat/repos')
+			.then((response) => response.json())
+			.then((data) => setRepositories(data));
+	}, []);
+
 	return (
 		<section className='repository-list'>
 			<h1>Repository List</h1>
+
 			<ul>
-				<RepositoryItem name='Repositório 1' />
-				<RepositoryItem name='Repositório 2' />
-				<RepositoryItem />
+				{repositories.map((repo) => (
+					<RepositoryItem
+						name={repo.name}
+						description={repo.description}
+						link={repo.html_url}
+						key={repo.id}
+					/>
+				))}
 			</ul>
 		</section>
 	);
